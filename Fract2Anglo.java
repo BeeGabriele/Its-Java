7e03d317880f623421be45bf8844ca0feea15c0a
BeeGabriele <96230170+BeeGabriele@users.noreply.github.com>
2022-01-21 15:53:49 +0100

    Versione Fract2Anglo 21.02.022
    Programma creato e funzionante

---
+++ Fract2Anglo.java
@@ -0,0 +1,19 @@
+/**
+* Inseriti due valori int <divisore> e <dividendo> viene stampato il risultato della frazione
+* in formato anglosassone
+*
+* @Author: Gabriele Bee		@Version: 21.02.2022
+**/
+import static java.lang.System.out;
+public class Fract2Anglo {
+	public static void main(String[] args){
+		if(args.length != 2){
+			out.print("Il programma richiede 2 argomenti, non di piu' e non di meno");
+		}else{
+			int dividendo= Integer.parseInt(args[0]);
+			int divisore= Integer.parseInt(args[1]);
+			double divisione= (double)dividendo/(double)divisore;
+			out.print("Il risultato della frazione e': " + divisione);
+		}
+	}
+}
