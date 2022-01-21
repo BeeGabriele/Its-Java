220914bbbafb29f06e685f9c029e83375b1a01cb
BeeGabriele <96230170+BeeGabriele@users.noreply.github.com>
2022-01-21 16:29:46 +0100

    Created GasStation
    To Do:
    - Add if 0

---
+++ GasStation.java
@@ -0,0 +1,29 @@
+/**
+* Viene stampata una pompa di benzina con il prezzo che sceglie l'utente al momento dell'avvio
+*
+* @Author: Gabriele Bee		 @Version 21.01.2022
+*
+**/
+public class GasStation{
+    public static void main (String[] args){
+        if(Integer.parseInt(args[0]) <= 999){
+            if(args.length != 1){
+                System.out.print("Errore: Il programma richiede 1 argomento");
+            }else{
+                int a = Integer.parseInt(args[0]) / 100;
+                int b = (Integer.parseInt(args[0]) - a * 100) / 10;
+                int c = (Integer.parseInt(args[0]) - a * 100 - b * 10);
+                System.out.println(" _________ ");
+                System.out.println("|  - - -  |");
+                System.out.println("| |" + a + "|" + b + "|" + c + "| |");
+                System.out.println("|  -.- -  |");
+                System.out.println("|_________|");
+                System.out.println(" |       | ");
+                System.out.println(" |       | ");
+                System.out.println("_|_______|_");
+	    }
+        }else{
+	    System.out.println("Il numero inserito e' troppo grande");
+        }
+    }
+}
